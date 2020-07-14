d14a5beb7499458e40145af64245699db21c62d1
Rachel Bauer <rachel.bauer1@gmail.com>
2020-07-14 19:48:56 -0400

    Fixed calculator validation

--- scripts/BauerBrewing.js
+++ scripts/BauerBrewing.js
@@ -28,36 +28,20 @@ window.onload = changeImg;
 
 
 /** ABV Calculator Formula
-ABV = 132.715(OG-FG) or (OG-FG)/.00753 **/
-
-var OG;
-var FG;
-var yourABV;
+ABV = 132.715(OG-FG) or (OG-FG)/.00753 - 1.040 **/
 
 function calculate(event) {
     event.preventDefault(); 
     var field1=document.getElementById('OG') .value;
     var field2=document.getElementById('FG') .value;
+    var OG=parseFloat(field1)
+    var FG=parseFloat(field2)
+    var result=(OG-FG)*131.25;
 
-    var result=(parseFloat(field1)-parseFloat(field2))*131.25;
-
-if(!isNaN(result))
-{
-     document.getElementById("yourABV").innerHTML="Your ABV is "+result;
-}
-
-}
-
-/**function calculate() {
-        if(isNaN(OG) || isNaN(FG)) {
-            alert('Invalid OG or FG. Please enter values between 1.001 and 1.100 Bx');
-            return;
-        }
-}
-
-function calculate(event) {
     if(OG < 1 || OG > 1.101 || FG < 1 || FG > 1.101) {
         alert('Invalid OG or FG. Please enter values between 1.001 and 1.100 Bx');
         return;
     }
-}**/
+
+     document.getElementById("yourABV").innerHTML="Your ABV is "+result;
+}
